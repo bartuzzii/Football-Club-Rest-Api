aaffe437a03050672d2b2f11f5a1de8ebda3291a
bartuzzi <bartlomiej.jedruszak@pollub.edu.pl>
2018-07-16 15:44:29 +0200

    Update

--- src/main/java/org/bj/footballclubrestapi/repositories/PlayerRepository.java
+++ src/main/java/org/bj/footballclubrestapi/repositories/PlayerRepository.java
@@ -2,8 +2,6 @@ package org.bj.footballclubrestapi.repositories;
 
 import org.bj.footballclubrestapi.models.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PlayerRepository extends JpaRepository<Player,Integer> {
 }
