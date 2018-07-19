e991da19bc3957370e94f288ef6d63920e8b5e42
bartuzzi <bartlomiej.jedruszak@pollub.edu.pl>
2018-07-19 13:05:45 +0200

    Tests

--- src/test/java/org/bj/footballclubrestapi/controllers/FootballClubResourcesTest.java
+++ src/test/java/org/bj/footballclubrestapi/controllers/FootballClubResourcesTest.java
@@ -1,5 +1,150 @@
-import static org.junit.Assert.*;
+package org.bj.footballclubrestapi.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.bj.footballclubrestapi.models.FootballClub;
+import org.bj.footballclubrestapi.models.Player;
+import org.bj.footballclubrestapi.repositories.FootballClubRepository;
+import org.bj.footballclubrestapi.repositories.PlayerRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.core.Is.is;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class FootballClubResourcesTest {
 
+    public static String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            final String jsonContent = mapper.writeValueAsString(obj);
+            return jsonContent;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private MockMvc mockMvc;
+    @Mock
+    FootballClubRepository footballClubRepository;
+
+    @Mock
+    PlayerRepository playerRepository;
+
+    @InjectMocks
+    FootballClubResources footballClubResources;
+
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(footballClubResources).build();
+    }
+
+    @Test
+    public void getAll() throws Exception {
+        List<FootballClub> footballClubs = new ArrayList<>();
+        footballClubs.add(new FootballClub(1,"Arsenal","Kowalski"));
+        footballClubs.add(new FootballClub(2,"Chelsea","Sarri"));
+
+        when(footballClubRepository.findAll()).thenReturn(footballClubs);
+        mockMvc.perform(get("/fbclubs").contentType(MediaType.APPLICATION_JSON_VALUE).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].name", is("Arsenal")))
+                .andExpect(jsonPath("$[0].coach", is("Kowalski")))
+                .andExpect(jsonPath("$[1].id", is(2)))
+                .andExpect(jsonPath("$[1].name", is("Chelsea")))
+                .andExpect(jsonPath("$[1].coach", is("Sarri")));
+        verify(footballClubRepository,times(1)).findAll();
+    }
+
+    @Test
+    public void addClub() throws Exception {
+        FootballClub footballClub = new FootballClub(1,"Chelsea","Sarri");
+
+        when(footballClubRepository.save(any(FootballClub.class))).thenReturn(footballClub);
+        mockMvc.perform(post("/fbclubs")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(footballClub)))
+                .andExpect(status().isCreated());
+        verify(footballClubRepository,times(1)).save(any(FootballClub.class));
+
+
+    }
+
+    @Test
+    public void findClubById() throws Exception {
+        Optional<FootballClub> footballClub = Optional.of(new FootballClub(1,"Chelsea","Sarri"));
+
+        when(footballClubRepository.findById(1)).thenReturn(footballClub);
+        mockMvc.perform(get("/fbclubs/{id}",1).contentType(MediaType.APPLICATION_JSON_VALUE).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id",is(1)));
+        verify(footballClubRepository,times(2)).findById(1);
+    }
+
+    @Test
+    public void deleteClubById() throws Exception {
+        Optional<FootballClub> footballClub = Optional.of(new FootballClub(1,"Chelsea","Sarri"));
+
+        when(footballClubRepository.findById(1)).thenReturn(footballClub);
+        mockMvc.perform(delete("/fbclubs/{id}",1).contentType(MediaType.APPLICATION_JSON_VALUE).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        verify(footballClubRepository,times(1)).deleteById(1);
+
+    }
+
+
+
+    @Test
+    public void addPlayer() throws Exception {
+        Optional<FootballClub> footballClub = Optional.of(new FootballClub(1,"Chelsea","Sarri"));
+        Optional<Player> player = Optional.of(new Player(1,"Hazard",10,"Winger"));
+
+        when(footballClubRepository.findById(1)).thenReturn(footballClub);
+        when(playerRepository.save(any(Player.class))).thenReturn(player.get());
+        mockMvc.perform(post("/fbclubs/1/players").contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(player.get())))
+                .andExpect(status().isCreated());
+        verify(playerRepository,times(1)).save(any(Player.class));
+
+    }
+
+    @Test
+    public void findPlayerById() throws Exception {
+        Optional<FootballClub> footballClub = Optional.of(new FootballClub(1,"Chelsea","Sarri"));
+        Optional<Player> player = Optional.of(new Player(1,"Hazard",10,"Winger"));
+
+        when(footballClubRepository.findById(1)).thenReturn(footballClub);
+        when(playerRepository.findById(1)).thenReturn(player);
+        mockMvc.perform(get("/fbclubs/1/players/1").contentType(MediaType.APPLICATION_JSON_VALUE).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is("Hazard")))
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.shirtNumber", is(10)))
+                .andExpect(jsonPath("$.position", is("Winger")));
+
+        verify(playerRepository,times(1)).findById(1);
+    }
 }
